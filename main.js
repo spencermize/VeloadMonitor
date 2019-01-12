0f930033182995f0d5a3a6687755c42f4559f840
spencermize <spence.bassman@gmail.com>
2019-01-12 14:42:37 -0500

    phew...major refactor

--- main.js
+++ main.js
@@ -13,24 +13,19 @@ let template = [];
 //hardware connections
 const SerialPort = require('serialport')
 const Readline = require('@serialport/parser-readline')
-const parser = new Readline()
-let SerPort;
+let SerPort = [];
 var currSpeed = 0;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 function createWindow () {
-	appIcon = new Tray('icon.png');
 	const express = require('express');
 	const cors = require('cors')
 	const exp = express();
 	const port = 3001;
-	buildPortList();
-	// Open the hardware
-	parser.on('data', function(data){
-		currSpeed = data;
-	})
+
+	setInterval(buildPortList,3000);
 	exp.get('/:action',cors(), function(req,res,next){
 		let data = "";
 		switch (req.params.action) {
@@ -46,6 +41,7 @@ function createWindow () {
 	})
 	exp.listen(port, () => {
 	  console.log(`Server listenening on ${port}`);
+	  createTray();
 	});
 	
 }
@@ -72,15 +68,16 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
-
-function createTray(template){
-	  // Create the browser window.
-	appIcon.setToolTip('Veload Monitor');
-	connect = connectionStatus.length>0 ? "Connected to " + connectionStatus : "Disconnected";
+function updateTray(){
+	connect = connectionStatus ? "Connected to " + connectionStatus : "Disconnected";
+	console.log("updateTray");
 	let contextMenu = Menu.buildFromTemplate([
 		{
 			label: 'Veload Monitor'
 		},
+		{
+			type: 'separator'
+		},		
 		{
 			label: 'Status:',
 			enabled: false,
@@ -90,49 +87,54 @@ function createTray(template){
 			type: 'separator'
 		},
 		{
-			label: 'Ports',
-			submenu: template
-		},
-		{
-			type: 'separator'
-		},
-		{
 			label: 'Quit',
 			role: 'quit'
 		}
 	])
-	// Call this again for Linux because we modified the context menu
-	return appIcon.setContextMenu(contextMenu)
+	appIcon.setContextMenu(contextMenu)
+}
+function createTray(){
+	appIcon = new Tray('icon.png');
+	appIcon.setToolTip('Veload Monitor');
 }
 
 function buildPortList(){
-	SerialPort.list(function (err, ports) {
+	if(!connectionStatus){
 		template = [];
-		ports.forEach(function(port) {
-			let status = connectionStatus == port.comName ? true : false;
-			template.push({
-				label: port.comName,
-				type: "radio",
-				checked: status,
-				click: function(m,b,e){
-					if(SerPort && SerPort.isOpen){
-						SerPort.close();
+		SerialPort.list(function (err, ports) {
+			ports.forEach(function(port) {
+				let sp = new SerialPort(port.comName, {autoOpen:false})
+				sp.on('open', function(err){
+					console.log("open " + this.path);
+				});
+				sp.on('error', function(err) {
+					console.log(err);
+					connectionStatus = "";
+				});
+				sp.open(function (err) {
+					if (err) {
+						console.log(err);
+						return;
 					}
-					SerPort = new SerialPort(m.label, { baudRate: 9600 })
-					SerPort.on('open', function(){
-						connectionStatus = m.label;
-						buildPortList();
-						createTray(template);
+					sp.once('data',function(data){
+						console.log("connected " + this.path);
+						connectionStatus = this.path;
+						updateTray();								
 					});
-					SerPort.on('error,close', function(err) {
+					sp.on('close', function(close) {
+						console.log(close);
 						connectionStatus = "";
-						buildPortList();
-						createTray(template)
 					});
-					SerPort.pipe(parser)
-				}
+				})
+				sp.pipe(parser);
+				SerPort.push(sp)
 			});
-			createTray(template);
+			updateTray();
 		});
-	});
+	}
 }
+const parser = new Readline();
+parser.on('data', function(data){
+	console.log(data);
+	currSpeed = data;
+})
